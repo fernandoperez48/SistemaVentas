08a4d99d79608f4f41294cc21904ef7e7d5c07c6
gili1010 <lucianog_332@hotmail.com>
2024-05-08 00:08:23 -0300

    actual

--- .history/compras/create_20240506165349.php
+++ .history/compras/create_20240506165349.php
@@ -67,7 +67,7 @@ include '../app/controllers/almacen/listado_de_productos_por_proveedor.php';
                                                         <option value="">Seleccionar proveedor</option> <!--Opción por defecto -->
                                                         <?php
                                                         foreach ($proveedores_datos as $proveedores_datos) { ?>
-                                                            <option value="<?php echo $proveedores_datos['id_proveedor']; ?>"><?php echo $proveedores_datos['nombre_proveedor']; ?></option>
+                                                            <option value="<?php echo $proveedores_datos['id_proveedor']; ?>"><?php echo $proveedores_datos['nombre_proveedor']; ?></option> ?>
                                                         <?php } ?>
                                                     </select>
 
