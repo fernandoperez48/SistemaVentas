1024ee8020bdd2d4ce600f182f1be903c73936c3
Jose Luis Nievas <joseluisnievas123456@gmail.com>
2024-11-03 04:14:28 -0300

    errores en login

--- app/controllers/login/ingreso.php
+++ app/controllers/login/ingreso.php
@@ -4,32 +4,30 @@ include_once '../../config.php';
 $email = $_POST['email'];
 $password_user = $_POST['password_user'];
 
-$contador = 0;
+// Realizamos la consulta para obtener el email en la base de datos
 $sql = "SELECT * FROM tb_usuarios WHERE email='$email'";
 $resultado = $mysqli->query($sql);
 
 if ($resultado->num_rows > 0) {
-    while ($usuario = $resultado->fetch_assoc()) {
-        $contador++;
-        $email = $usuario['email'];
-        $nombres = $usuario['nombres'];
-        $password_user_tabla = $usuario['password_user'];
-    }
+    // Creamos un array para almacenar los datos del usuario
+    $usuario = $resultado->fetch_assoc();
 
-    if ($contador > 0 && password_verify($password_user, $password_user_tabla)) {
-        echo "Datos Correctos";
+    // Obtenemos la contraseña guardada en la base de datos
+    $password_user_tabla = $usuario['password_user'];
+
+    // Verificamos que la contraseña ingresada coincida con la de la base de datos
+    if (password_verify($password_user, $password_user_tabla)) {
+        //if ($password_user === $password_user_tabla) { // temporal para prueba    
         session_start();
         $_SESSION['email'] = $email;
-        header('location:' . $URL . '/index.php');
+
+        // Devolvemos 'success' para indicar que los datos son correctos
+        echo "success";
     } else {
-        echo "No existe el usuario...";
-        session_start();
-        $_SESSION['mensaje'] = "Error: datos incorrectos";
-        header('location:' . $URL . '/login/index.php');
+        // Si la contraseña no coincide
+        echo "errorpass"; // Cambiado de "Contraseña incorrecta." a "errorpass"
     }
 } else {
-    echo "No existe el usuario...";
-    session_start();
-    $_SESSION['mensaje'] = "Error: usuario no encontrado";
-    header('location:' . $URL . '/login/index.php');
+    // Si no existe el usuario en la base de datos
+    echo "usuario_no_encontrado";
 }
--- login/index.php
+++ login/index.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+  <?php include_once '../app/config.php'; ?>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Sistema de Ventas</title>
@@ -20,49 +22,37 @@
       background-repeat: no-repeat;
       background-position: center;
     }
+
     .login-box {
-      background-color: black; /* Cambiar opacidad si es necesario */
+      background-color: black;
+      /* Cambiar opacidad si es necesario */
       border-radius: 10px;
       box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
       padding: 10px;
     }
+
     .login-box img {
       width: 300px;
       opacity: 0.8;
     }
   </style>
 </head>
+
 <body class="hold-transition login-page">
-<div class="login-box">
-  <!-- /.login-logo -->
+  <div class="login-box">
 
-  <?php 
-  session_start();
-  if(isset($_SESSION['mensaje'])){
-    $respuesta=$_SESSION['mensaje'];
-    ?>
-    <script>
-    Swal.fire({
-      position: 'top-end',
-      icon: 'error',
-      title: '<?php echo $respuesta ?>',
-      showConfirmButton: false,
-      timer: 1500
-    })
-    </script>
-    <?php
-  }
-  ?>
-  <div class="card card-outline card-primary" style="border-color: black;">
-    <div class="card-header text-center bg-light">
-      <a href="" class="h1">Sistema de VENTAS</a>
-    </div>
-    <div class="card-body">
-      <h3 class="login-box-msg">Ingrese sus datos</h3>
+    <div class="card card-outline card-primary" style="border-color: black;">
+      <div class="card-header text-center bg-light">
+        <a href="" class="h1">Sistema de VENTAS</a>
+      </div>
 
-      <form action="../app/controllers/login/ingreso.php" method="post">
+      <div class="card-body">
+        <h3 class="login-box-msg">Ingrese sus datos</h3>
         <div class="input-group mb-3">
-          <input type="email" name="email" class="form-control" placeholder="Email" style="border-color: black;">
+          <input type="email" id="email" class="form-control" placeholder="Email" style="border-color: black;">
+          <br>
+
+
           <div class="input-group-append">
             <div class="input-group-text" style="border-color: black;">
               <span class="fas fa-envelope"></span>
@@ -70,7 +60,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" name="password_user" class="form-control" placeholder="Password" style="border-color: black;">
+          <input type="password" id="password_user" class="form-control" placeholder="Password" style="border-color: black;">
           <div class="input-group-append">
             <div class="input-group-text" style="border-color: black;">
               <span class="fas fa-lock"></span>
@@ -79,27 +69,74 @@
         </div>
         <div class="row">
           <div class="col-8">
-           
           </div>
-          <!-- /.col -->
           <div class="col-12">
-            <button type="submit" class="btn btn-warning btn-block">Ingresar</button>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
-    </div>
-    <!-- /.card-body -->
-  </div>
-  <!-- /.card -->
-</div>
-<!-- /.login-box -->
+            <button type="button" class="btn btn-success btn-block" id="btn_ingresar">Ingresar</button>
 
-<!-- jQuery -->
-<script src="../public/templates/AdminLTE-3.2.0/plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="../public/templates/AdminLTE-3.2.0/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="../public/templates/AdminLTE-3.2.0/dist/js/adminlte.min.js"></script>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+  <!-- jQuery -->
+  <script src="../public/templates/AdminLTE-3.2.0/plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="../public/templates/AdminLTE-3.2.0/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="../public/templates/AdminLTE-3.2.0/dist/js/adminlte.min.js"></script>
+  <script>
+    const baseURL = "<?php echo $URL; ?>"; // Pasar la URL desde PHP a JavaScript
+
+    $('#btn_ingresar').click(function() {
+      let email = $('#email').val();
+      let contraseña = $('#password_user').val();
+
+      // Validaciones
+      function validarEmail(email) {
+        let re = /\S+@\S+\.\S+/;
+        return re.test(email);
+      }
+
+      if (!validarEmail(email)) {
+        alert('Debes escribir un correo electrónico.');
+      } else if (contraseña === '') {
+        //si contraseña esta vacia
+        alert('La contraseña esta vacia');
+      } else {
+        let formData = new FormData();
+        formData.append('email', email);
+        formData.append('password_user', contraseña);
+
+        $.ajax({
+          url: '../app/controllers/login/ingreso.php',
+          type: 'POST',
+          data: formData,
+          contentType: false,
+          processData: false,
+          success: function(datos) {
+            if (datos === "success") { // Asegúrate de que ingreso.php devuelva "success" en caso de éxito
+              // Redirigir a la página deseada usando la URL base
+              window.location.href = baseURL + '/index.php';
+            } else if (datos === "usuario_no_encontrado") { // Nueva condición para manejar el error
+              alert("Usuario no encontrado");
+            } else if (datos === "errorpass") { // Nueva condición para manejar el error
+              alert("Contraseña incorrecta.");
+            } else {
+              alert("Credenciales incorrectas o error en el inicio de sesión.");
+            }
+          },
+          error: function() {
+            alert("Hubo un problema con el servidor. Por favor, inténtelo nuevamente.");
+          }
+        })
+      }
+    })
+  </script>
+
 </body>
+
 </html>
