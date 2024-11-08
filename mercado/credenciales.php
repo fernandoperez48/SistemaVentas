53f1dad5a6cb4ce8bea76fb629ed9b5e202a791c
Jose Luis Nievas <joseluisnievas123456@gmail.com>
2024-11-07 22:29:07 -0300

    credenciales

---
+++ mercado/credenciales.php
@@ -0,0 +1,47 @@
+identificacion de cuenta => fa_comprador
+nombre de usuario => TESTUSER1813216812
+contraseña => lJzJq9zrtD
+
+
+TARJETA DE CREDITO
+
+MASTERCARD
+NUMERO: 5031 7557 3453 0604
+CODIGO DE SEGURIDAD: 123
+FECHA DE CADUCIDAD: 11/25
+
+VISA
+NUMERO: 4509 9535 6623 3704
+CODIGO DE SEGURIDAD: 123
+FECHA DE CADUCIDAD: 11/25
+
+AMERICAN EXPRESS
+NUMERO: 3711 803032 57522
+CODIGO DE SEGURIDAD: 1234
+FECHA DE CADUCIDAD: 11/25
+
+
+Estado de pago Descripción Documento de identidad
+APRO
+Pago aprobado
+(DNI) 12345678
+OTHE
+Rechazado por error general
+(DNI) 12345678
+CONT
+Pendiente de pago
+-
+CALL
+Rechazado con validación para autorizar
+-
+FUND
+Rechazado por importe insuficiente
+-
+SECU
+Rechazado por código de seguridad inválido
+-
+EXPI
+Rechazado debido a un problema de fecha de vencimiento
+-
+FORM
+Rechazado debido a un error de formulario
